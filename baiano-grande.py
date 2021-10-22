382c57fc3b5a4258dcb64db0703d5bd1ee78acda
joajoajpedroj <eugomes.jp@gmail.com>
2021-10-22 11:08:49 -0300

    Cleanup and wrapping things to classes

--- baiano-grande.py
+++ baiano-grande.py
@@ -4,20 +4,37 @@ from pygame import mixer
 mixer.init()
 mixer.music.load("audio.mp3")
 
-def on_press(key):
-    if key == keyboard.Key.home:
-        return False  # stop listener
-    try:
-        k = key.char  # single-char keys
-    except:
-        k = key.name  # other keys
-    if k in ['1', '2', 'left', 'right']:  # keys of interest
-        mixer.music.play()
-        # self.keys.append(k)  # store it in global-like variable
-        print('Key pressed: ' + k)
-        #return False  # stop listener; remove this if want more keys
+class Audio:
+	def __init__(self, file):
+		self.file = file
+		
+class Trigger:
+	def __init__(self, stopKey = keyboard.Key.home, triggerKeys = ['down'], files = []):
+		self.stopKey = stopKey
+		self.triggerKeys = triggerKeys
+		self.files = files
+	
+	def on_press(self, key):
+		# Stop if stopKey is pressed
+		if key == self.stopKey:
+			return False
+		# Get the key
+		try:
+			k = key.char
+		except AttributeError:
+			k = key.name
+		# What to do if one of triggerKeys is pressed
+		if k in self.triggerKeys:
+			print("DEBUG: '{}' was pressed and will trigger the audio".format(k))
+			
 
-# Collect events until released
-with keyboard.Listener(
-        on_press=on_press) as listener:
-    listener.join()
+def main():
+	Triggerer = Trigger()
+
+	# Collect events until released
+	with keyboard.Listener(
+			on_press=Triggerer.on_press) as listener:
+		listener.join()
+
+if __name__ == "__main__":
+	main()
